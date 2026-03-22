eb13f6dd296b1496b109301d040980246fb100a6
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2026-03-22 23:25:37 +0700

    test

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NotFound() {
   return (
     <div>
-      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
+      <h1 className="text-4xl font-bold mb-4">404 - Page Not Found hihihi</h1>
       <p className="text-lg text-gray-600">The page you are looking for does not exist.</p>
     </div>
   )
